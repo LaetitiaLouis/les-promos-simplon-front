9e4dc030af94d58ec5efadc1e4143d2df7b1619b
LOUISL <laetitia.louis.lp4@gmail.com>
2020-02-04 11:16:11 +0100

    install material

---
+++ src/app/utilisateur.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilisateurService {
+BASE_URL = 'http://localhost:8080/api/utilisateurs'
+  constructor(private http: HttpClient) {
+  }
+  getAllUsers() {
+    return this.http.get(`${this.BASE_URL}/all`);
+  }
+
+  getUserById(id) {
+    return this.http.get(`${this.BASE_URL}/findById?id=${id}`);
+  }
+}
