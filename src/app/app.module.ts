f716551e525756cc0b09c0d626ab68c52c782d8e
LOUISL <laetitia.louis.lp4@gmail.com>
2020-02-04 11:20:22 +0100

    Mise en place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   declarations: [
     AppComponent
--- src/app/utilisateur.service.ts
+++ src/app/utilisateur.service.ts
@@ -4,6 +4,7 @@ import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
 export class UtilisateurService {
 BASE_URL = 'http://localhost:8080/api/utilisateurs'
   constructor(private http: HttpClient) {
